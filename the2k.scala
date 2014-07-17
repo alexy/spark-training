dc33e6cde3558d986043a277deac98c64d432bae
Alexy Khrabrov <deliverable@gmail.com>
2014-07-16 20:22:46 -0700

    sync

--- the2k.scala
+++ the2k.scala
@@ -1,19 +1,32 @@
-import org.apache.spark.sql._ 
-val ctx = new org.apache.spark.sql.SQLContext(sc) 
-val tweets = sc.textFile("hdfs:/twitter") 
-val tweetTable = JsonTable.fromRDD(sqlContext, tweets, Some(0.1)) 
-tweetTable.registerAsTable("tweetTable") 
+import org.apache.spark.sql._
+import org.apache.spark.mllib.linalg.{Vector,Vectors}
+import org.apache.spark.mllib.clustering.KMeans 
 
-ctx.sql("SELECT text FROM tweetTable LIMIT 5").collect.foreach(println) 
-ctx.sql("SELECT lang, COUNT(*) AS cnt FROM tweetTable \
+val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+val tweets = sqlContext.jsonFile("/data/tweets100K.json")
+// tweets.printSchema()
+tweets.registerAsTable("tweets") 
+
+sqlContext.sql("SELECT text FROM tweets LIMIT 5").collect.foreach(println) 
+sqlContext.sql("SELECT lang, COUNT(*) AS cnt FROM tweets \
   GROUP BY lang ORDER BY cnt DESC LIMIT 10").collect.foreach(println) 
-val texts = sql("SELECT text FROM tweetTable").map(_.head.toString) 
+val texts = sqlContext.sql("SELECT text FROM tweets WHERE text IS NOT NULL").map(_.head.toString) 
 
-def featurize(str: String): Vector = { ... } 
+def featurize(str: String): Vector = {
+    val n = 1000
+    val result = new Array[Double](n)
+    val bigrams = str.sliding(2).toArray
+    for (h <- bigrams.map(_.hashCode % n)) {
+        result(h) += 1.0 / bigrams.length
+    }
+    Vectors.sparse(n, result.zipWithIndex.filter(_._1 != 0).map(_.swap))
+} 
+
+
 val vectors = texts.map(featurize).cache() 
 val model = KMeans.train(vectors, 10, 10) 
 
-sc.makeRDD(model.clusterCenters, 10).saveAsObjectFile("hdfs:/model")
+sc.makeRDD(model.clusterCenters, 10).saveAsObjectFile("/data/tweets/model100K.rdd")
 val ssc = new StreamingContext(new SparkConf(), Seconds(1)) 
   
 val model = new KMeansModel(
